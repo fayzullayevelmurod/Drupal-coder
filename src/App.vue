cca2a489adc98dc554c5ae59446f9597464f6cf2
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-01-05 19:51:58 +0500

    comment card

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="comment_container">
+    <h1 class="comment_title">
+    Отзывы
+  </h1>
+  <img src="./assets/bg_left.png" alt="" class="bg_left">
   <swiper
     :pagination="{
       type: 'fraction',
@@ -7,12 +12,62 @@
     :modules="modules"
     class="mySwiper"
   >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
-    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+    <swiper-slide>
+      <div class="comment">
+        <div class="comment_left">
+          <img src="./assets/comment_logo.svg" alt="">
+          <h6>
+            Команда Drupal Coder вызвала только положительные впечатления!
+          </h6>
+          <p>
+            Нуреев Александр, менеджер проекта Winamp Russian Community
+          </p>
+        </div>
+        <div class="line"></div>
+        <div class="comment_right">
+          
+        </div>
+
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="comment">
+        <div class="comment_left">
+          <img src="./assets/comment_logo.svg" alt="">
+          <h6>
+            Команда Drupal Coder вызвала только положительные впечатления!
+          </h6>
+          <p>
+            Нуреев Александр, менеджер проекта Winamp Russian Community
+          </p>
+        </div>
+        <div class="line"></div>
+        <div class="comment_right">
+
+        </div>
+
+      </div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="comment">
+        <div class="comment_left">
+          <img src="./assets/comment_logo.svg" alt="">
+          <h6>
+            Команда Drupal Coder вызвала только положительные впечатления!
+          </h6>
+          <p>
+            Нуреев Александр, менеджер проекта Winamp Russian Community
+          </p>
+        </div>
+        <div class="line"></div>
+        <div class="comment_right">
+
+        </div>
+
+      </div>
+    </swiper-slide>
   </swiper>
+  </div>
 </template>
 <script>
   // Import Swiper Vue.js components
@@ -39,3 +94,78 @@
     },
   };
 </script>
+
+
+<style scoped>
+
+.comment_container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+.comment_title {
+  color: #050C33;
+  text-align: center;
+  font-family: 'Montserrat',sans-serif;
+  font-size: 42px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding-top: 60px;
+  margin-bottom: 46px;
+}
+
+.comment {
+  padding: 80px 80px 88px 120px;
+  margin: 0 auto;
+  max-width: 980px;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.10);
+  background: #FFF;
+  box-shadow: 0px 4px 60px 0px rgba(161, 161, 161, 0.25);
+}
+
+.line {
+  width: 10px;
+  background: #000;
+  opacity: 0.2;
+  height: 100%;
+}
+
+.swiper-pagination-fraction {
+  position: absolute;
+  top: 0;
+}
+
+.comment_left h6 {
+  max-width: 440px;
+  margin: 41px 0 28px 0;
+  color: #050C33;
+  font-family: 'Montserrat',sans-serif;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-decoration-line: underline;
+}
+
+.comment_left p {
+  max-width: 380px;
+  color: #050C33;
+  font-family: 'Montserrat',sans-serif;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.mySwiper {
+  width: 100%;
+  border: 1px solid red;
+}
+
+.bg_left {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+</style>
